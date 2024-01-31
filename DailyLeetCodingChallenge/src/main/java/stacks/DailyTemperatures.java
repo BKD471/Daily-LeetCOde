add10dd09e2b39a1b088ceb16cad6aa8fc6c22e9
BKD471 <bhaskarkumardas9@gmail.com>
2024-01-31 21:07:21 +0530

    d31

--- DailyLeetCodingChallenge/src/main/java/stacks/DailyTemperatures.java
+++ DailyLeetCodingChallenge/src/main/java/stacks/DailyTemperatures.java
@@ -1,4 +1,21 @@
 package stacks;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 public class DailyTemperatures {
+    public int[] dailyTemperatures(int[] nums) {
+        int n=nums.length;
+        int[] res=new int[n];
+        Arrays.fill(res,0);
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[st.peek()]<=nums[i]) st.pop();
+
+            if(!st.isEmpty() && nums[st.peek()]>nums[i]) res[i]=st.peek()-i;
+            st.push(i);
+        }
+        return res;
+    }
 }
